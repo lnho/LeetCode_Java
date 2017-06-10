504928acd42c972a5bc545b42fcc1cbb2f8ec7c7
LINHAO <lin.hao71@zte.com.cn>
2017-06-10 13:30:19 +0800

    Solution098

--- src/main/java/com/lnho/leetcode/solution/Solution098.java
+++ src/main/java/com/lnho/leetcode/solution/Solution098.java
@@ -13,23 +13,32 @@ import com.lnho.leetcode.common.TreeNode;
  */
 public class Solution098 {
     public boolean isValidBST(TreeNode root) {
-        return validate(root, null);
+        return validate(root, null, null);
     }
 
-    public boolean validate(TreeNode current, TreeNode parent) {
+    public boolean validate(TreeNode current, Integer min, Integer max) {
         if (current == null) return true;
-        if (current.left != null && (current.left.val >= current.val || (parent != null && current.left.val <= parent.val)))
-            return false;
-        if (current.right != null && (current.right.val <= current.val || (parent != null && current.right.val >= parent.val)))
-            return false;
-        if (validate(current.left, current) && validate(current.right, current)) return true;
-        return false;
+        if (current.left != null) {
+            int leftMax = (max == null || current.val < max) ? current.val : max;
+            if (current.left.val >= leftMax || (min != null && current.left.val <= min))
+                return false;
+            if (!validate(current.left, min, leftMax))
+                return false;
+        }
+        if (current.right != null) {
+            int rightMin = (min == null || current.val > min) ? current.val : min;
+            if (current.right.val <= rightMin || (max != null && current.right.val >= max))
+                return false;
+            if (!validate(current.right, rightMin, max))
+                return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(1);
-        TreeNode treeNode2 = new TreeNode(1);
-        treeNode.left = treeNode2;
+        TreeNode treeNode = new TreeNode(-2147483648);
+        TreeNode treeNode2 = new TreeNode(2147483647);
+        treeNode.right = treeNode2;
         System.out.println(new Solution098().isValidBST(treeNode));
     }
 }
